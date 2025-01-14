8b6c28c41397f7b609722654a4ae8214fca1a5b5
yashaa17 <yvsadabal@gmail.com>
2025-01-14 10:41:58 +0530

    Collections

---
+++ StylishPen/src/com/app/utils/PenUtility.java
@@ -0,0 +1,51 @@
+package com.app.utils;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import com.app.pen.Pen;
+
+
+public class PenUtility {
+	public static void addDiscount(List<Pen> penList) {
+		for (Pen pen : penList) {
+			int year=Period.between(pen.getUpdateDate(),LocalDate.now()).getYears();
+			
+			if(Period.between(pen.getUpdateDate(),LocalDate.now()).getMonths()+year*12>=3) {
+				
+				double price= pen.getPrice();
+				double disc=price*.20;
+				pen.setDiscount(disc);
+				
+			}
+			
+		}
+		
+	}
+	public static String updatePenStock(Pen pen,int stock,List<Pen> penList) {
+		int index=penList.indexOf(pen);
+		Pen pen1=penList.get(index);
+		pen1.setStock(stock+pen1.getStock());
+		pen1.setUpdateDate(LocalDate.now());
+		
+		return "Pen stock updated successfully";
+	}
+	public static void removePen(List<Pen> penList) {
+		
+		Iterator<Pen> itr=penList.iterator();
+		while(itr.hasNext()) {
+			int year=Period.between(itr.next().getUpdateDate(),LocalDate.now()).getYears();
+			if(Period.between(itr.next().getUpdateDate(),LocalDate.now()).getMonths()+year*12>=9)
+			{
+				itr.remove();
+			}
+		}
+//		for (Pen pen : penList) {
+//			if(Period.between(pen.getUpdateDate(),LocalDate.now()).getMonths()>=9) {
+//				
+//			}
+	}
+}
